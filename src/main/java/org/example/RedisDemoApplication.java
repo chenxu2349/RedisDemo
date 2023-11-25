f8df0bbf8aea134461ba96017e924a375aceb20b
chenxu <chenxu_2349@qq.com>
2023-11-25 13:22:26 +0800

    prepare work2

--- src/main/java/org/example/RedisDemoApplication.java
+++ src/main/java/org/example/RedisDemoApplication.java
@@ -1,11 +1,18 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisDemoApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(RedisDemoApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(RedisDemoApplication.class, args);
+        logger.info("****************************************\n");
+        logger.info("    RedisDemo Service has started...\n");
+        logger.info("****************************************");
     }
 }
